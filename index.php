d59ee7729570c98bce440376d8a5b3007b9cd738
Arnaud <arnaud.banvillet@gmail.com>
2013-06-25 08:11:19 +0200

    Init of the theme base on _s

---
+++ index.php
@@ -0,0 +1,23 @@
+/**
+ * Main template
+ *
+ */
+
+get_header();
+?>
+<div id="content" class="site-content" role="main">
+
+		<?php
+		if ( have_posts() ){
+			while ( have_posts() ){
+				the_post();
+				get_template_part( 'content', 'page');
+
+			}
+
+		}
+		?>
+		</div><!-- #content -->
+
+<?php
+get_footer();
